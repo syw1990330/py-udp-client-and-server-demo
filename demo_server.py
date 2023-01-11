e438bce49b4178ad6ca1a7fdac5993209ee6d522
syw1990330 <74365330+syw1990330@users.noreply.github.com>
2023-01-11 12:50:22 +0800

    Update demo_server.py

--- demo_server.py
+++ demo_server.py
@@ -1,18 +1,6 @@
 import socket
 import threading
 
-# def client_recv(client,address):
-#     print(address,'client address')
-#     with client:
-#         while True:
-#             data = client.recv(1024)
-#             if not data:
-#                 break
-#             client.sendall(data)
-    
-#     print(f'client address:{address} broken')
-
-
 with socket.socket(socket.AF_INET,socket.SOCK_DGRAM) as socket_server:
     socket_server.bind(("0.0.0.0",1234)) #0.0.0.0 represents acceptting any ip
 
